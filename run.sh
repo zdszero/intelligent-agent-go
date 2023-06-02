00fc80285b5d877ff427f226f69475576f2e35cb
yw <yfshangan@163.com>
2023-06-02 12:45:58 +0800

    create ns and configmap in run.sh

--- run.sh
+++ run.sh
@@ -19,10 +19,26 @@ PROXY_SERVICE="proxy-service"
 CLUSTER_SERVICE="cluster-service"
 SELECTOR_APP="proxy-app"
 NAMESPACE="smart-agent"
+CONFIGMAP="client-map"
+K="minikube kubectl --"
+
+createNsCm() {
+    ns_cnt=$($K get ns | grep ${NAMESPACE} | wc -l)
+    if [[ $ns_cnt -eq 0 ]]; then
+        echo "create namespace ${NAMESPACE}"
+        $K create ns ${NAMESPACE}
+    fi
+    cm_cnt=$($K get configmap -n ${NAMESPACE} | grep ${CONFIGMAP} | wc -l)
+    if [[ $cm_cnt -eq 0 ]]; then
+        echo "create configmap ${CONFIGMAP}"
+        $K create configmap ${CONFIGMAP} --from-file=configmap.yaml -n ${NAMESPACE}
+    fi  
+}
 
 if [[ "$1" == "build" ]]; then
     echo "Running build operation..."
     eval $(minikube docker-env)
+    createNsCm
     go build -o server cmd/server/main.go
     docker build -t my-agent .
     num=$2
@@ -33,35 +49,35 @@ if [[ "$1" == "build" ]]; then
         sed -i "s/${PROXY_SERVICE}/\0${i}/" $deploy_temp
         sed -i "s/${CLUSTER_SERVICE}/\0${i}/" $deploy_temp
         sed -i "s/${SELECTOR_APP}/\0${i}/" $deploy_temp
-        minikube kubectl -- apply -f $deploy_temp
+        $K apply -f $deploy_temp
         rm $deploy_temp
     done
 elif [[ "$1" == "show" ]]; then
     echo "Services":
-    minikube kubectl -- get svc -n ${NAMESPACE}
+    $K get svc -n ${NAMESPACE}
     echo "Deployments":
-    minikube kubectl -- get deployment -n ${NAMESPACE}
+    $K get deployment -n ${NAMESPACE}
 elif [[ "$1" == "clean" ]]; then
     echo "Running clean operation..."
-    running_services=$(minikube kubectl -- get svc -n $NAMESPACE | grep $PROXY_SERVICE | awk '{print $1}')
+    running_services=$($K get svc -n $NAMESPACE | grep $PROXY_SERVICE | awk '{print $1}')
     while IFS= read -r service; do
         deploy=$(echo $service | sed 's/service/deployment/')
-        minikube kubectl -- delete service $service -n $NAMESPACE
-        minikube kubectl -- delete deployment $deploy -n $NAMESPACE
+        $K delete service $service -n $NAMESPACE
+        $K delete deployment $deploy -n $NAMESPACE
     done <<< $running_services
-    running_services=$(minikube kubectl -- get svc -n $NAMESPACE | grep $CLUSTER_SERVICE | awk '{print $1}')
+    running_services=$($K get svc -n $NAMESPACE | grep $CLUSTER_SERVICE | awk '{print $1}')
     while IFS= read -r service; do
-        minikube kubectl -- delete service $service -n $NAMESPACE
+        $K delete service $service -n $NAMESPACE
     done <<< $running_services
 elif [[ "$1" == "log" ]]; then
     [[ $# < 2 ]] && exit 0
-    podname=$(minikube kubectl -- get pods -n ${NAMESPACE} | grep "${DEPLOY}${2}" | awk '{print $1}')
+    podname=$($K get pods -n ${NAMESPACE} | grep "${DEPLOY}${2}" | awk '{print $1}')
     # echo $podname
-    minikube kubectl -- logs -f $podname -n ${NAMESPACE}
+    $K logs -f $podname -n ${NAMESPACE}
 elif [[ "$1" == "ssh" ]]; then
     [[ $# < 2 ]] && exit 0
-    podname=$(minikube kubectl -- get pods -n ${NAMESPACE} | grep "${DEPLOY}${2}" | awk '{print $1}')
-    minikube kubectl -- exec -it $podname -n ${NAMESPACE} -- /bin/sh
+    podname=$($K get pods -n ${NAMESPACE} | grep "${DEPLOY}${2}" | awk '{print $1}')
+    $K exec -it $podname -n ${NAMESPACE} -- /bin/sh
 else
     printHelp
 fi
